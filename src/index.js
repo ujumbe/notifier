93981eb18ff3b9bda8ed007d953e92f0dc68937b
Antony Waiganjo <ganjotony@gmail.com>
2023-11-08 21:10:52 +0300

    Refactored invoices querying

--- src/index.js
+++ src/index.js
@@ -1,41 +1,61 @@
 import { MongoClient } from 'mongodb';
-
-const uri = 'mongodb://localhost:27017';
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+import 'dotenv/config';
 
 async function getDB() {
   try {
+    const uri = process?.env?.MONGODB_URI;
+    const client = new MongoClient(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+
     await client.connect();
-    return client.db('kaende');
+    return client.db(process?.env?.MONGODB_NAME);
   } catch (ex) {
-    console.error(ex);
+    console.error({ ex });
   }
 }
 
-export const start = async () => {
-  console.log('Connecting to DB...');
-  const db = await getDB();
+console.log('Connecting to DB...');
+const db = await getDB().catch((error) => {
+  console.error(error);
+});
 
+export const start = async () => {
   const runNotificationCycle = async () => {
-    const invoices = await db.collection('invoices').find({}).toArray();
-    const tasks = invoices.map(
-      (invoice) =>
-        new Promise((resolve, reject) => {
-          const { dueDate } = invoice;
-          const today = Date.now();
-          if (today <= dueDate) {
-            sendNotification(invoice).then(resolve).catch(reject);
-          } else {
-            resolve();
+    try {
+      const invoices = await db
+        .collection(process?.env?.MONGODB_INVOICES)
+        .find({
+          $expr: {
+            $eq: [
+              { $dateToString: { format: '%Y-%m-%d', date: '$$NOW' } },
+              {
+                $dateToString: { format: '%Y-%m-%d', date: '$dueDate' },
+                notificationSent: false
+              }
+            ]
           }
         })
-    );
-    await Promise.all(tasks);
-  };
+        .toArray();
 
+      if (!Array.isArray(invoices)) {
+        console.error('No invoices found');
+        return;
+      }
+
+      const tasks = invoices.map(
+        (invoice) =>
+          new Promise((resolve, reject) => {
+            sendNotification(invoice).then(resolve).catch(reject);
+          })
+      );
+
+      await Promise.allSettled(tasks);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   console.log('Starting notification cycle...');
-  setInterval(runNotificationCycle, 86400000);
+  setInterval(runNotificationCycle, process?.env?.CYCLE_INTERVAL);
 };
